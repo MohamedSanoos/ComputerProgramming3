a7f797eb07734bfe49f35439f653abf2905fc379
MohamedSanoos <sanoos78@gmail.com>
2020-09-18 06:07:17 +0530

    Setters and getters are added from phys branch

--- CscExamples.java
+++ CscExamples.java
@@ -3,4 +3,28 @@ class CscExamples{
 	String category;
 	String text;
 	
+	void setQNum(int qNum){
+		this.qNum = qNum;
+		
+	}
+	
+	void setcategory(String category){
+		this.category = category;
+	}
+	
+	void setText(String setText){
+		this.text = text;
+	}
+	
+	int getQNum(){
+		return qNum;
+	}
+	
+	String getCategory(){
+		return category;
+	}
+	
+	String getText(){
+		return text;
+	}
 }
